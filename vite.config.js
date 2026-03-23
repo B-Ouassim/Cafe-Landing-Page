ccb7a96b14e57008d1b291c5bdb31c6731e86390
Ouassim Babakhali <babakhaliouassim@gmail.com>
2026-03-23 15:32:00 +0100

    m

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
-// https://vite.dev/config/
+
 export default defineConfig({
+  base: '/Cafe-Landing-Page/',  // important: repo name with slashes
   plugins: [react(), tailwindcss()],
-  base: "/Cafe-landingPage/"
 })
